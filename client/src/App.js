aa24395172d941598b50aa274e3f10d63ce3a461
Bhuvaneswari <hire.bhuvana@gmail.com>
2024-03-15 12:20:56 +0530

    first

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {Routes, Route} from "react-router-dom";
+import User from './components/getuser/User';
+import Add from './components/adduser/Add';
+import Edit from './components/updateuser/Edit';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<User />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/edit/:id" element={<Edit />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
